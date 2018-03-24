5a4ba2789dfa95f346af3c0c7b30b21c548aaffc
Ian Canino <ian.canino@gmail.com>
2018-03-24 00:54:51 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,24 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+var index = 0
+function konamiCodeHandler(e) { 
+  console.log(index)
+  var key = e.which
+  console.log(code)
+  console.log(key)
+  
+  if (key === code[index]) {
+    console.log(index)
+    
+    if (index === (code.length)) {
+      alert("Hurray")
+      index = 0
+    }
+    index++
+  } else {
+    index = 0
+  }
+}
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
+  document.body.addEventListener("keydown", konamiCodeHandler)
 }
