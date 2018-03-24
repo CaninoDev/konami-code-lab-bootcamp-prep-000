71a123e2764e481685d50b7b6a3ec5b12e118abf
Ian Canino <ian.canino@gmail.com>
2018-03-24 01:00:51 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 var index = 0
 function konamiCodeHandler(e) { 
-  var key = e.which
+  var key = e.code
   console.log(code)
   console.log("KeyPress: " + key)
   
