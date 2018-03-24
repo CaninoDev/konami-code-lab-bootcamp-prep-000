5c80550982980dfdf97a32bdcbd6815a79dd1774
Ian Canino <ian.canino@gmail.com>
2018-03-24 01:24:39 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ var index = 0
 
 function init() {
   document.body.addEventListener("keydown", function(e) { 
-  var key = e.which
+  var key = e.keyCode
   console.log(code)
   console.log("KeyPress: " + parseInt(key))
   
